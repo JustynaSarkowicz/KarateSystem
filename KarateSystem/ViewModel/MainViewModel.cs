aed0511e5dfda1b4fa053679fcd48a23b6e57e85
Justyna Sarkowicz <justyna.sarkowicz@elte-s.com>
2025-03-14 14:37:07 +0100

    Conected MainWindow with Childs

---
+++ KarateSystem/ViewModel/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿using FontAwesome.Sharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using System.Windows.Input;
+
+namespace KarateSystem.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        //Fields
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
+        //Properties
+        public ViewModelBase CurrentChildView
+        {
+            get
+            {
+                return _currentChildView;
+            }
+
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+
+        public string Caption
+        {
+            get
+            {
+                return _caption;
+            }
+
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+
+        public IconChar Icon
+        {
+            get
+            {
+                return _icon;
+            }
+
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+        //--> Commands
+        public ICommand ShowHomeViewCommand { get; }
+        public ICommand ShowCustomerViewCommand { get; }
+
+        public MainViewModel()
+        {
+            //Initialize commands
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
+
+            //Default view
+            ExecuteShowHomeViewCommand(null);
+        }
+
+        private void ExecuteShowCustomerViewCommand(object obj)
+        {
+            CurrentChildView = new CompetitorsViewModel();
+            Caption = "Zawodnicy";
+            Icon = IconChar.UserGroup;
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Strona Główna";
+            Icon = IconChar.Home;
+        }
+    }
+}
